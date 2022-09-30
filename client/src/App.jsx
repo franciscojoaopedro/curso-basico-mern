7ec76688c220d18ef46c2a2ee925db54d427b394
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-30 22:47:45 +0100

    atualizar as rotas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 
 import Inicio from "./pages/client/painel/index";
+import Routas from './routes/routes';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      {/* <h1>CURSO BÁSICO DE MERN</h1> */}
-      <Inicio/>
+    <div>
+      <Routas/>
     </div>
   )
 }
